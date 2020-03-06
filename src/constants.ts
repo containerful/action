18edc411e3861a407b55e6343667d53b54e79d6c
remorses <beats.by.morse@gmail.com>
2020-03-06 14:18:42 +0100

    made deploy

---
+++ src/constants.ts
@@ -0,0 +1,3 @@
+import path from 'path'
+
+export const CONTAINERFUL_BIN = require.resolve('containerful/bin.js')
---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import core from '@actions/core'
+import { exec } from '@actions/exec'
+import { ExecOptions } from '@actions/exec/lib/interfaces'
+import { CONTAINERFUL_BIN } from './constants'
+
+async function deploy() {
+    const options: ExecOptions = {
+        // cwd: '.',
+        errStream: process.stderr,
+        outStream: process.stdout,
+    }
+    const dockerComposePath = core.getInput('docker_compose_path')
+    const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN', { required: true })
+    await exec(CONTAINERFUL_BIN, ['login', '--github-token', GITHUB_TOKEN], options)
+    await exec(CONTAINERFUL_BIN, ['deploy', '-f', dockerComposePath], options)
+}
+
+
+deploy()
