85377bcc17776a5762f65ac7dcd113646e6898e4
remorses <beats.by.morse@gmail.com>
2020-03-06 14:19:10 +0100

    compile

---
+++ dist/constants.d.ts
@@ -0,0 +1,1 @@
+export declare const CONTAINERFUL_BIN: string;
---
+++ dist/constants.js
@@ -0,0 +1,3 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.CONTAINERFUL_BIN = require.resolve('containerful/bin.js');
